1fec286e9d119407273c914d99442c73a1a4abd6
Confusified <60018584+Confusified@users.noreply.github.com>
2025-02-11 21:13:11 +0100

    fix error, ' is not allowed

--- StrongerHostVotingPower/Configuration/Config.cs
+++ StrongerHostVotingPower/Configuration/Config.cs
@@ -19,7 +19,7 @@ public static class Config
         ConfigDescription votingPowerDescription = new("Determines how many votes will be added when you vote (as a host)", new AcceptableValueRange<int>(1, 100));
 
         useCustomVotingPower = cfg.Bind("Global Settings", "Use Custom Voting Power", true, "Enables custom voting power for the host. (You must be the host)");
-        limitCustomVotes = cfg.Bind("Global Settings", "Don't Go Over Vote Threshold", true, "When enabled, your voting power will be limited by the total required amount of votes");
+        limitCustomVotes = cfg.Bind("Global Settings", "Do Not Go Over Vote Threshold", true, "When enabled, your voting power will be limited by the total required amount of votes");
         customVotingPower = cfg.Bind("Voting Settings", "Custom Voting Power", 2, votingPowerDescription);
         usePercentageOfTotalVotesRequired = cfg.Bind("Percentage Voting Settings", "Use Percentage Of Total Required Votes", false, "When enabled, your voting power will be equal to the percentage of required total votes");
         customVotingPowerPercentage = cfg.Bind("Percentage Voting Settings", "Custom Voting Power Percentage", 50, votingPowerDescription);
